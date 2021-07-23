1e4f0a2862ecb744bcbe9e9400dad5e6423af603
Gabriel Rocha <gabrielsrgbi@gmail.com>
2021-07-23 16:33:09 -0300

    Alterado

--- files/urls.py
+++ files/urls.py
@@ -5,8 +5,16 @@ from . import views
 urlpatterns = [
     path('', views.files, name='files'),
     path('files/', views.files, name='files'),
+    
+    path('view/<int:id>/', views.fileView, name='file-view'),
     path('files/view/<int:id>/', views.fileView, name='file-view'),
+    
+    path('newFile/', views.newFile, name='new-file'),
     path('files/newFile/', views.newFile, name='new-file'),
+    
+    path('edit/<int:id>', views.editFile, name='edit-file'),
     path('files/edit/<int:id>', views.editFile, name='edit-file'),
+    
+    path('delete/<int:id>', views.deleteFile, name='delete-file'),
     path('files/delete/<int:id>', views.deleteFile, name='delete-file'),
 ]
