228e879a8f0c8ae2fd484f9d8dd79e9f295ba3e2
Gabriel Rocha <gabrielsrgbi@gmail.com>
2021-07-29 21:33:23 -0300

    delete file com modal inserido

--- files/static/_js/_dltFile.js
+++ files/static/_js/_dltFile.js
@@ -1,12 +1,30 @@
-const btn_dlt = document.querySelector('.fa-times-circle');
-const nav_msg = document.querySelector('.messages');
-
-
-btn_dlt.addEventListener("click", () => {
+$( document ).ready(function() {
     
-    console.log("Funcionou");
-    nav_msg.style.display ="none"
+    const btn_link = document.querySelector('.delete-btn');
+    const modal = document.querySelector('.box-modal');
+    const btn_csl = document.querySelector('.dlt-btn-modal-csl');
+    const btn_cfm = document.querySelector('.dlt-btn-modal-cfm');
+    
+    $(btn_link).on('click', function(e) {
 
-})
+        e.preventDefault();
+        modal.style.display = "flex"
+        var dltLink = $(this).attr('href');
+
+        $(btn_cfm).on('click', function() {
     
+            console.log("deletar");
+            window.location.href = dltLink;
     
+        });
+        
+        $(btn_csl).on('click', function() {
+    
+            console.log("cancelar");
+            modal.style.display = "none"
+    
+        });
+    });
+
+    
+});
