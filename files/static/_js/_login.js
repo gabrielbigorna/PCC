e1815672253ada8bbde90cbbb37b1f5807788880
Gabriel Rocha <gabrielsrgbi@gmail.com>
2021-07-29 21:56:30 -0300

    Alterações de cadastro

--- files/static/_js/_login.js
+++ files/static/_js/_login.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     const label = document.querySelectorAll('label.requiredField');
     const div = document.querySelectorAll('div.form-group');
     const input = document.querySelectorAll('.textinput.textInput.form-control');
-      
+    const small = document.querySelectorAll('.form-text.text-muted');
+  
     
     label[0].style.display = 'none'
     input[0].placeholder = 'Usuário'
@@ -15,6 +16,15 @@ $( document ).ready(function() {
     input[1].placeholder = 'Senha'
     input[1].classList.toggle('index')
     div[1].classList.toggle('index')
+
+    label[2].style.display = 'none'
+    input[2].placeholder = 'Confirmar Senha'
+    input[2].classList.toggle('index')
+    div[2].classList.toggle('index')
+
+    small[0].style.display = 'none'
+    small[1].style.display = 'none'
+    small[2].style.display = 'none'
     
     
 });
