dca24bf3b3afc69dea260ab3029ab60799268f34
Gabriel Rocha <gabrielsrgbi@gmail.com>
2021-08-01 14:01:58 -0300

    Page.views adicionado

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import PageForm
+from .models import Page
+from boxes.models import Box
 
 # Create your views here.
+@login_required
+def pages(request, id):
+
+    page = get_object_or_404(Page, pk=id)
+    
+    boxes = Box.objects.all().filter(user=request.user, id=id)
+
+    return render(request, 'pages/pages.html', {'page': page, 'boxes': boxes})
+
+@login_required
+def newPages(request, id):
+       
+    if request.method == 'POST':
+        boxes = Box.objects.all().filter(user=request.user, id=id)
+        form_page = PageForm(request.POST)
+
+        if form_page.is_valid():
+            form_page.save()
+
+        else:
+            return redirect('new-page', id=boxes.id)
+        
+        
+        return redirect('/boxes')
+
+    else:
+        
+        boxes = Box.objects.all().filter(user=request.user, id=id)
+        
+        pages = Page.objects.all().filter(ident=id)
+        
+        form_page = PageForm()
+
+        return render(request, 'pages/newPages.html', {'form_page': form_page, 'boxes': boxes, 'pages': pages})  
+
