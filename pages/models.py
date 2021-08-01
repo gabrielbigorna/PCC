7d7dd26f2ad0b8606ca9685870ec7bdcdd2e7074
Gabriel Rocha <gabrielsrgbi@gmail.com>
2021-08-01 14:08:07 -0300

    Pages.models adicionado

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from boxes.models import Box
 
 # Create your models here.
+class Page(models.Model):
+
+    ident = models.OneToOneField(Box, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.ident.title;
